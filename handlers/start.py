7081320eff820f17e6de968ff49b6623d146e791
Belov Dmitrii <belovd90@mail.ru>
2025-06-18 16:45:23 +0300

    добавлены кнопки меню в tg боте

--- handlers/start.py
+++ handlers/start.py
@@ -1,9 +1,17 @@
-from telegram import Update
+from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import ContextTypes
+
+main_menu_keyboard = [
+    ["➕ Добавить", "📋 Список"],
+    ["🗑 Удалить", "⚙ Настройки"]
+]
+
+main_menu_markup = ReplyKeyboardMarkup(main_menu_keyboard, resize_keyboard=True)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "Привет! Я твой ToDo-бот.\n"
         "Используй команду /list, чтобы посмотреть задачи.\n"
-        "И команду /help, чтобы посмотреть все возможные команды."
+        "И команду /help, чтобы посмотреть все возможные команды.",
+        reply_markup=main_menu_markup
     )
